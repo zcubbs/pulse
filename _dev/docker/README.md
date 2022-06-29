be74976fc7d53c5409efeeab9698d11aa671db87
zcubbs <52750533+zcubbs@users.noreply.github.com>
2022-06-30 01:22:22 +0200

    :art: reorganize docker dev assets

--- _dev/docker/README.md
+++
@@ -1,6 +0,0 @@
-## Run docker compose
-
-```bash
-docker stack down pulse-tools
-docker stack deploy --compose-file docker-compose.tools.yaml pulse-tools
-```
--- _dev/docker/run.ps1
+++
@@ -1,3 +0,0 @@
-docker stack down pulse-tools
-$filebase = Join-Path $PSScriptRoot "docker-compose.tools.yaml"
-docker stack deploy --compose-file $filebase pulse-tools
